b4f0bf357abcf7ce9596ec5195807798154489ac
kolkol69 <kolodiy1103@gmail.com>
2018-03-18 20:05:59 +0100

    added database basic comunication client->database

---
+++ resources/js/db_update.js
@@ -0,0 +1,23 @@
+
+function writeUserData(firstname, lastname, participantID, participantResult) {
+    firebase.database().ref('participants/' + participantID).set({
+        firstname,
+        lastname,
+        participantID,
+        participantResult,
+    });
+}
+$(function () {
+    
+    var database = firebase.database();
+    
+    const button_ = $("#submit_button");
+    console.log(button_);
+    $("#submit_button").click(function(){
+        console.log("data sent to DB! YOLO");
+        writeUserData('maks', 'kol', '12', '01:00:18');
+    });
+});
+
+
+
