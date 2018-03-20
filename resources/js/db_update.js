4ce5cd2f9613847c6f079f7feeddee7ca3e28237
kolkol69 <kolodiy1103@gmail.com>
2018-03-20 14:26:02 +0100

    Settings: added some basic rank creation form.

--- resources/js/db_update.js
+++ resources/js/db_update.js
@@ -1,23 +1,23 @@
 
-// function writeUserData(firstname, lastname, participantID, participantResult) {
-//     firebase.database().ref('participants/' + participantID).set({
-//         firstname,
-//         lastname,
-//         participantID,
-//         participantResult,
-//     });
-// }
-// $(function () {
+function writeUserData(firstname, lastname, participantID, participantResult) {
+    firebase.database().ref('participants/' + participantID).set({
+        firstname,
+        lastname,
+        participantID,
+        participantResult,
+    });
+}
+$(function () {
     
-//     var database = firebase.database();
+    var database = firebase.database();
     
-//     const button_ = $("#submit_button");
-//     console.log(button_);
-//     $("#submit_button").click(function(){
-//         console.log("data sent to DB! YOLO");
-//         writeUserData('maks', 'kol', '12', '01:00:18');
-//     });
-// });
+    const button_ = $("#submit_button");
+    console.log(button_);
+    $("#submit_button").click(function(){
+        console.log("data sent to DB! YOLO");
+        writeUserData('maks', 'kol', '12', '01:00:18');
+    });
+});
 
 
 
