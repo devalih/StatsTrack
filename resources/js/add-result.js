b9ad32fe54e0692b241efb989a5959a4b347fa68
kolkol69 <kolodiy1103@gmail.com>
2018-03-31 20:54:56 +0200

    back up version :c

--- resources/js/add-result.js
+++ resources/js/add-result.js
@@ -1,30 +1,3 @@
-function upd_table() {
-    // e.preventDefault();
-    $(".competitors-table").empty();
-    $.ajax({
-        url: '/participants',
-        success: function (result) {
-            console.log('AJAX result',result);
-            result.forEach((elem) => {
-                let firstname = JSON.stringify(elem.fname).replace(/"/g, ""),
-                    lastname = JSON.stringify(elem.lname).replace(/"/g, ""),
-                    routes = JSON.stringify(elem.finishedRoutes).replace(/"/g, ""),
-                    result = JSON.stringify(elem.result);
-                $(".competitors-table").append(
-                    `<tr>
-                        <td>${firstname}</td> 
-                        <td>${lastname}</td>
-                        <td>${routes}</td> 
-                        <td>${result}</td>    
-                    </tr>`);
-            });
-        },
-        error: function (request, status, error) {
-            serviceError();
-        }
-    });
-}
-
 $(function () {
     $("#addResult_button").click(() => {
         const route = $('#select-walls :selected').text(),
@@ -34,12 +7,14 @@ $(function () {
 
         // console.log('eeee,cos: ',route,points,participant,fname,lname);
         $.post('/add_result',
-            { fname, lname, route, points, },
-            (data) => {
-                if (data.success) {
-                    upd_table();
+            {
+                fname,
+                lname,
+                route,
+                points,
+            }, (data) => {
+                if (data.success)
                     alert('Result was updated');
-                }
                 else
                     alert('Result was not updated, try again');
             });
